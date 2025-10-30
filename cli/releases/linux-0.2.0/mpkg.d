4293c4c69c25a3af68814c1ec024034dc0e6c425
Nayte Swart <seradedstripes@gmail.com>
2025-10-30 11:43:52 +0200

    remove now useless releases folder

--- cli/releases/linux-0.2.0/mpkg.d
+++
@@ -1,1 +0,0 @@
-/proj/npm/mpkg/cli/target/x86_64-unknown-linux-gnu/release/mpkg: /proj/npm/mpkg/cli/src/main.rs
--- cli/releases/windows-0.2.0/mpkg.d
+++
@@ -1,1 +0,0 @@
-/proj/npm/mpkg/cli/target/x86_64-pc-windows-gnu/release/mpkg.exe: /proj/npm/mpkg/cli/src/main.rs
